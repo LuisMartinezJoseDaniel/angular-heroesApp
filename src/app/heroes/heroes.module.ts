b2fc95e8bdce3b980e53c05cff8f97e97657a3da
Jose Daniel Luis Martinez <josedan.jd94@gmail.com>
2023-06-30 18:31:36 -0600

    Agregar buscador con autocomplete, agregar formulario de heroe

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
-import { MaterialModule } from '../material/material.module';
 import { CardComponent } from './components/card/card.component';
+
 import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 @NgModule({
@@ -22,6 +25,11 @@ import { HeroImagePipe } from './pipes/hero-image.pipe';
     // Pipe
     HeroImagePipe,
   ],
-  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    HeroesRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule, // Para trabajar con formularios reactivos
+  ],
 })
 export class HeroesModule {}
--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NewPageComponent {
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
 
+  public publishers = [
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' },
+  ];
+
+  constructor(private heroesService: HeroesService) {}
+
+  onSubmit(): void {
+    if (!this.heroForm.valid) return;
+    // Observables: Cualquier observable si no se suscribe, no se dispara.
+    if (this.currentHero.id) {
+      this.heroesService.updateHero(this.currentHero).subscribe((hero) => {
+        //TODO mostrar snackbar
+      });
+      return;
+    }
+    this.heroesService.addHero(this.currentHero).subscribe((hero) => {
+      //TODO mostrar snackbar y navegar a /heroes/edit/hero.id
+    });
+  }
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
 }
